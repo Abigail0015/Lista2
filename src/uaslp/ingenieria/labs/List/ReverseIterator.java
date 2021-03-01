569fc70251da69c89e27405453371902460de29b
Abigail0015 <A298360@alumnos.uaslp.mx>
2021-02-28 22:00:06 -0600

    tarea 5

---
+++ src/uaslp/ingenieria/labs/List/ReverseIterator.java
@@ -0,0 +1,22 @@
+package uaslp.ingenieria.labs.List;
+
+public class ReverseIterator
+{
+
+
+    public ReverseIterator(Node tail)
+    {
+
+    }
+
+    public boolean hasNext()
+    {
+
+        return false;
+    }
+
+    public int next(){
+
+        return 0;
+    }
+}
